141eab986e9dc55958d33fe732e3fe24cf98d269
Davashdgreat <109927389+Davashdgreat@users.noreply.github.com>
2024-10-21 23:57:12 +0100

    Environment Setup
    Prepared the codebase environment for work

---
+++ school-field-rental/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Booking from './pages/Booking';
+
+
+const app = () =>{
+  return (
+    <Router>
+      <div>
+        {'Routes'}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/booking" element={<Booking />} />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+export default App;
